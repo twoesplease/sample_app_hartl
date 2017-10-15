34158f76e8e4c8df5061a44f690009648642982a
Tonee Young <itisi@toneeyoung.com>
2017-10-14 22:50:53 -0400

    Allow users to edit their profiles, authorize user before allowing edit, use friendly forwarding for non-logged-in users

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,13 +1,13 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  test "invalid signup information" do
+  test 'invalid signup information' do
     get signup_path
     assert_no_difference 'User.count' do
-      post users_path, params: { user: { name: "",
-                                  email: "user@invalid",
-                                  password:              "foo",
-                                  password_confirmation: "bar" } }
+      post users_path, params: { user: { name: '',
+                                         email: 'user@invalid',
+                                         password:              'foo',
+                                         password_confirmation: 'bar' } }
     end
     assert_template 'users/new'
     assert_select 'div#error_explanation'
@@ -15,13 +15,13 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'form[action="/signup"]'
   end
 
-  test "valid signup information" do
+  test 'valid signup information' do
     get signup_path
     assert_difference 'User.count', 1 do
-     post users_path, params: { user: { name: "Example User",
-                                        email: "user@example.com",
-                                        password:              "password",
-                                        password_confirmation: "password" } }
+      post users_path, params: { user: { name: 'Example User',
+                                         email: 'user@example.com',
+                                         password:              'password',
+                                         password_confirmation: 'password' } }
     end
     follow_redirect!
     assert_template 'users/show'
